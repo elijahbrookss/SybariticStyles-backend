7096e5088e32ad95242cb821ec19c7c79cc74db6
elijahcbrooks <elijahcbrooks@gmail.com>
2021-03-07 12:50:26 -0600

    comments controller and routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :get_comment, only: [:destroy, :update]
+
+  def index
+    comments = Comment.all
+    render json: comments
+  end
+
+  def create
+    comment = Comment.new(comments_params)
+    if comment.save
+      render json: comment
+    end
+  end
+
+  def destroy
+    if @comment
+      @comment.destroy
+    end
+  end
+
+  def update
+    if @comment
+      @comment.update(comments_params)
+      render json: @comment
+    end
+  end
+
+  private
+
+  def get_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comments_params
+    params.require(:comment).permit(:user_id, :merchandise_id, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :destroy, :show, :update]
   resources :carts, only: [:create, :destroy]
+  resources :comments, only: [:index, :create, :destroy, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
